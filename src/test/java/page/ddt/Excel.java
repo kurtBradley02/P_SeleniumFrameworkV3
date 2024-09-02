707fb24ebe0bdc93fe5f830cddbe13a72950023c
DisIsNotMe <orumighto02@gmail.com>
2024-09-02 14:15:24 +0800

    [DDT] - added excel reader

--- src/test/java/page/ddt/Excel.java
+++ src/test/java/page/ddt/Excel.java
@@ -1,9 +1,42 @@
 package page.ddt;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
 public class Excel {
 	
-	public static void read() {
+	
+	//static String filePath = "C:/Users/kurtb/OneDrive/Desktop/feature ddt/P_SeleniumFrameworkV3/resources/testdata.xlsx";
+	
+	
+	static String filePath = System.getProperty("user.dir") + "/resources/testdata.xlsx";
+
+	public static String read(String sheetName, int rowNumber, int columnNumber) {
 		
+        String value = null;
+
+        try (FileInputStream fileInputStream = new FileInputStream(filePath);
+             Workbook workbook = new XSSFWorkbook(fileInputStream)) {
+
+            Sheet sheet = workbook.getSheet(sheetName);
+            if (sheet != null) {
+                Row row = sheet.getRow(rowNumber);
+                if (row != null) {
+                    Cell cell = row.getCell(columnNumber);
+                    if (cell != null) {
+                        value = cell.toString();  // Convert the cell value to string
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return value;
 	}
 	
 	public static void write() {
--- src/test/java/page/tests/TestPage.java
+++ src/test/java/page/tests/TestPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
 
+import page.ddt.Excel;
 import page.object.base.TestBase;
 import page.object.models.PageLogin;
 import page.reports.ReportJira;
@@ -21,25 +22,42 @@ public class TestPage extends TestBase{
 	@BeforeMethod
 	public void testConfig() {
 		page = new PageLogin(driver);
-	}
-	
-	@Test(priority = 1)
-	public void navigateTo() {
 		page.driver.get("file:///C:/Users/kurtb/OneDrive/Desktop/Login.html");
 	}
+
 	
-	@Test(priority = 2)
-	public void userCanLoginWithValidCredentials() throws InterruptedException, IOException
+	@Test(priority = 1)
+	public void userLoginValidCredentials() throws InterruptedException, IOException
 	{
 		try {
 		
-			page.getUsername().sendKeys("admin");
-			page.getPassword().sendKeys("123456");
+			page.getUsername().sendKeys(Excel.read("LoginScenario", 1, 2));
+			page.getPassword().sendKeys(Excel.read("LoginScenario", 1, 3));
 			page.getLoginButton().click();
 			
 			WebElement successLabel = explicitWait(By.xpath("//*[@id=\"successModal\"]/div/h3"));
 			
 			assertEquals(successLabel.getText(), "Login Success");
+		}catch(Exception e) {
+			ReportJira.generate("userCanLoginWithValidCredentials", driver, e);
+			
+			throw e;
+		}
+
+	}
+	
+	@Test(priority = 2)
+	public void userLoginInvalidUsername() throws InterruptedException, IOException
+	{
+		try {
+		
+			page.getUsername().sendKeys(Excel.read("LoginScenario", 2, 2));
+			page.getPassword().sendKeys(Excel.read("LoginScenario", 2, 3));
+			page.getLoginButton().click();
+			
+			WebElement failLabel = explicitWait(By.xpath("//*[@id=\"failModal\"]/div/h3"));
+			
+			assertEquals(failLabel.getText(), "Login Fail");
 		}catch(Exception e) {
 			ReportJira.generate("userCanLoginWithValidCredentials", driver, e);
 			
